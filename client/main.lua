0e3cb709b090844f440eef6b970f9a707f7dfae7
Loafabreadly <birdgeekthree@gmail.com>
2026-01-03 10:59:04 -0700

    feat: implement client-side revive functionality

--- client/main.lua
+++ client/main.lua
@@ -1,2 +1,16 @@
--- Placeholder
-print("RP-Alpha Ambulance Loaded")
+-- Revive Logic
+RegisterNetEvent('rpa-ambulance:client:revive', function()
+    local ped = PlayerPedId()
+    
+    -- Framework specific revive usually
+    -- Here we do basic:
+    local res = GetEntityCoords(ped)
+    NetworkResurrectLocalPlayer(res.x, res.y, res.z, GetEntityHeading(ped), true, false)
+    SetEntityHealth(ped, 200)
+    ClearPedBloodDamage(ped)
+    
+    exports['rpa-lib']:Notify("You have been revived", "success")
+end)
+
+-- Interaction to revive someone (Target -> Server -> Client)
+-- Here we simulate the receiving end
